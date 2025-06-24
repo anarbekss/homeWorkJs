4a2aa0744a951ab8fe75ea6770e09b17b8df6b30
anrbekss <anarbek3312@gmail.com>
2025-06-24 19:52:12 +0600

    everything is finish

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 // var itsNumber = 1
+// console.log(itsNumber);
 // console.log(itsNumber.toString());
 
 // console.log(Boolean(itsNumber));
+// console.log(Boolean(!itsNumber));
+// console.log(itsNumber * 'sstj');
 
 
 // ///////////////////////////
@@ -11,6 +14,9 @@
 // console.log(Number(itsString));
 
 // console.log(Boolean(itsString));
+// console.log(Boolean(!itsString));
+// console.log(itsString / 5);
+
 
 
 // ///////////////////////////
@@ -18,17 +24,27 @@
 // var itsBoolean = true
 // console.log(Number(itsBoolean));
 // console.log(itsBoolean.toString());
+// console.log(!itsBoolean);
 
 
 // ///////////////////////////
+var itsUndefined
 
-// console.log(itsUndefined);
+    console.log(itsUndefined);
 
-var firstNumber = Number(prompt("напишите сумму"))
 
-if (firstNumber > 1000 || firstNumber > 5000 ) {
-    var discount = firstNumber / 100 * 5
-    console.log('Вы получаете 5% процентную скидку в размере'+ ' '+ Math.ceil(discount));
-} else { 
-    console.log('у вас не хватает денег чтобы получить скидку');                       
-}
+
+// var firstNumber = Number(prompt("Напишите сумму"));
+
+// if (firstNumber > 10000) {
+//     var discount15 = firstNumber / 100 * 15
+//     console.log('Вы получаете 15% скидку в размере ' + discount15.toFixed(2));
+// } else if (firstNumber > 5000) {
+//     var discount10 = firstNumber / 100 * 10
+//     console.log('Вы получаете 10% скидку в размере ' + discount10.toFixed(2));
+// } else if (firstNumber > 1000) {
+//     var discount5 = firstNumber / 100 * 5
+//     console.log('Вы получаете 5% скидку в размере ' + discount5.toFixed(2));
+// } else {
+//     console.log('У вас не хватает денег, чтобы получить скидку');
+// }
